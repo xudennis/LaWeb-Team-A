dcd1488264f7c16116b4edfb0bb9f727e7f342a0
Dennis Xu <xudennis168@gmail.com>
2016-03-01 08:57:33 -0800

    8:55am 03/01

--- job.js
+++ job.js
@@ -7,11 +7,18 @@ function plusDivs(n) {
 
 function showDivs(n) {
   var i;
-  var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = x.length} ;
+  // var x = document.getElementsByClassName("mySlides");
+
+  var x = document.getElementById("slides");
+  if (n > x.length) {
+  	slideIndex = 1}    
+
+  if (n < 1) {
+  	slideIndex = x.length} ;
+
   for (i = 0; i < x.length; i++) {
      x[i].style.display = "none";  
   }
+
   x[slideIndex-1].style.display = "block";  
 }
